9e2349992e1f93f9dc64b654f2363b51c3a1db96
bob <xhban520@gmail.com>
2016-06-17 14:59:14 +0800

    modiftied

--- .gollum.sh
+++ .gollum.sh
@@ -1,2 +1,2 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-/usr/local/bin/gollum --css --allow-uploads=dir --collapse-tree --port 4567 $DIR
+/usr/local/bin/gollum --css --allow-uploads=page --h1-title --collapse-tree --port 4567 $DIR
